9ae0984105b404e4938f33c2ad654d421c838448
Matthew Wolffsohn <matthew@wolfie.ovh>
2018-06-05 21:09:48 +0100

    First commit
    (Well, technically not, but I accidentally commmited the bot token last time)

---
+++ README.md
@@ -0,0 +1,3 @@
+# OverDisc2
+
+A complete rewrite of OverDisc in order to implement new functionality and teach myself a new language.
---
+++ overdisc.js
@@ -0,0 +1,65 @@
+const owjs = require('overwatch-js');
+const redis = require("redis");
+const Redisclient = redis.createClient();
+const Discord = require("discord.js");
+const Discordclient = new Discord.Client();
+const fs = require("fs");
+
+const configFile = JSON.parse(fs.readFileSync("config.json", "utf8"));
+
+Redisclient.on("ready", () => console.log("Redis Ready"));
+
+Redisclient.on("error", (err) => console.log("Error " + err));
+
+Discordclient.on('ready', () => console.log(`Logged in as ${Discordclient.user.tag}!`));
+
+Discordclient.on('message', msg => {
+	if (msg.content.startsWith === configFile.delim || msg.author.bot) return;
+	// Only look at messages with our delim and not from a bot
+	const args = msg.content.slice(configFile.delim.length).split(' ');
+	const command = args.shift().toLowerCase();
+	if (command === `ping`) {
+		console.log(`${msg.author.tag} has just pinged us!`);
+		console.log(`Their ID is ${msg.author.id}`);
+		msg.channel.send('pong');
+	}
+
+	else if (command === `config`) {
+		if (!args.length) {
+			return msg.channel.send(`Command help: ${configFile.delim}config\n\noverwatchtag - Sets your Overwatch tag`)
+		}
+		// We're configuring someone.  Let's find out what setting they are using
+		if (args[0].toLowerCase() === "overwatchtag") {
+			// Let's set their Overwatch tag
+			if (args[1]) {
+				Redisclient.hset(`users:${msg.author.id}`, `Overwatch_Tag`, args[1]);
+				msg.channel.send(`Your overwatch tag has been set to ${args[1]} ✅`);
+			}
+			else if (Redisclient.hexists(`users:${msg.author.id}`, `Overwatch_Tag`)) {
+				Redisclient.hget(`users:${msg.author.id}`, `Overwatch_Tag`, function(err, reply) {
+					const overwatchtag = reply;
+					msg.channel.send(`Your current Overwatch tag is ${overwatchtag}`);
+				});
+			}
+			else {
+				msg.channel.send(`Command help: ${configFile.delim}config overwatchtag <tag>\n\n<tag> - Your Overwatch tag`);
+			}
+
+		}
+		else {
+			msg.channel.send(`Command help: ${configFile.delim}config
+			\n
+			\n
+			overwatchtag - Sets your Overwatch tag`)
+		}
+	}
+
+	else if (command === 'args-info') {
+		if (!args.length) {
+			return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
+		}
+		msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	}
+});
+
+Discordclient.login(configFile.bot_token);
